984363606f13a9b5f6dd471b8a81bb1b8e14b97f
Jack Jin <hanchong1996@hotmail.com>
2017-11-24 02:43:48 -0500

    after routing

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DataService } from './services/data.service';
+import { FrormsModule } from '@angular/forms';
 
+import { routing } from './app.routes';
+
+import { DataService } from './services/data.service';
 
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
 
 
 @NgModule({
@@ -14,10 +18,13 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     AppComponent,
     ProblemListComponent,
     ProblemDetailComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NewProblemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule
   ],
   providers: [
     DataService
--- week1/oj-client/src/app/app.routes.ts
+++ week1/oj-client/src/app/app.routes.ts
@@ -3,23 +3,24 @@ import { ProblemListComponent } from './components/problem-list/problem-list.com
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes [
-  {
-    path :'',
-    redirectTo: 'problems',
-    pathMatch: 'full'
-  },
-  {
-    path: 'problems',
-    component: ProblemListComponent;
-  },
-  {
-    path: 'problems/:id',
-    component:  ProblemDetailComponent;
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent;
-  }
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'problems',
+        pathMatch: 'full'
+    },
+    {
+        path: 'problems',
+        component: ProblemListComponent
+    },
+    {
+        path: 'problems/:id',
+        component: ProblemDetailComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'problems'
+    }
+];
 
-]
+export const routing = RouterModule.forRoot(routes);
