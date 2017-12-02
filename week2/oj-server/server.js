35c7252abee75f015d046bfd33b7b4844e8363ae
Jack Jin <hanchong1996@hotmail.com>
2017-12-02 17:57:33 -0500

    added mongodb

--- week2/oj-server/server.js
+++ week2/oj-server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const restRouter = require('./routes/rest');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://user:user@ds125716.mlab.com:25716/onlinejudge');
 
-app.use('/api/v1/', restRouter);
+app.use('/api/v1', restRouter);
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
